efdd0d7e64d5699fb7d14607f3855ab2f187ac13
usamarazzaq90 <usamarazzaq9014@gmail.com>
2025-01-20 10:02:49 +0500

    completed part 15

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express()
 const userModel=require("./models/user") //Getting the schema from user model
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 const path=require('path')
 const cookieParser = require('cookie-parser')
@@ -23,16 +24,45 @@ app.post('/create', (req,res)=>{
 
     bcrypt.genSalt(10,(err,salt)=>{
         bcrypt.hash(password, salt, async(err,hash)=>{
-            let createdUer=await userModel.create({
+            let createdUser=await userModel.create({
                 username,
                 email,
                 password: hash,
                 age
             })
         
-            res.send(createdUer)
+            let token=jwt.sign({email},"shhhh");
+            res.cookie("token", token)
+            res.send(createdUser)
         })      
     })
+})
+
+//Creating login
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.post('/login',async (req,res)=>{
+    let user=await userModel.findOne({email: req.body.email});
+    // console.log(user)
+    if(!user) return res.send("Something went wrong!")
+    
+    // console.log(user.password, req.body.password)
+    bcrypt.compare(req.body.password, user.password, (err,result)=>{
+        if(result) {
+            let token=jwt.sign({email: user.email},"shhhh");
+            res.cookie("token", token)
+            res.send("You can login");
+        }
+        else res.send("Something went wrong!")
+    })
+})
+
+//Creating logout
+app.get('/logout',(req,res)=>{
+    res.cookie("token","")
+    res.redirect('/')
 })
 
 app.listen(3000)
